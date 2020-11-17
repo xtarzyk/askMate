20070d8c947f665cb444f7f942fda10cbcf1bbea
Fraktalia <g.w.koscielniak@gmail.com>
2020-11-17 11:54:15 +0100

    New.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, request
+import random
+import string
 
 app = Flask(__name__)
 saved_data = {}
@@ -14,10 +16,29 @@ def hello():
 def add_question():
     if request.method == 'POST':
         question['note'] = request.form['note']
-
+        id = id_maker()
         return redirect('/question/<question_id>')
     return render_template('index.html')
 
+
+def id_maker():
+    ide = []
+    allowed_special_chars = "_", "+", "-", "!"
+
+    for letter in range(0, 6):
+        if letter < 4:
+            ide.append(([random.choice(string.ascii_lowercase)]))
+        else:
+            ide.append(([random.choice(string.ascii_uppercase)]))
+            ide.append(([random.choice(string.digits)]))
+            ide.append([random.choice(allowed_special_chars)])
+    random.shuffle(ide)
+    all_ide = []
+    for let in range(len(ide)):
+        all_ide.append(ide[let][0])
+    in_ide = ''.join(all_ide)
+
+    return in_ide
 
 
 if __name__ == "__main__":
