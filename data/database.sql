3ac7c444036ebbeb02d3d0809288e624d0d11d65
Fraktalia <g.w.koscielniak@gmail.com>
2020-12-01 19:46:54 +0100

    Main page

--- data/database.sql
+++ data/database.sql
@@ -29,10 +29,18 @@ CREATE TABLE answers
 );
 
     INSERT INTO questions
-    VALUES (1, 'Chcę na święta upiec dobry piernik dla 5 osób. Czy ktoś z Was ma jakiś ciekawy przepis dla początkującego kucharza?', 1493368154, 'Jak zrobić dobry piernik?', 5, 3),
-            (2, 'Cześć. Pierwszy raz zrobiłam dzisiaj świąteczne ciasteczka z przepisu Pani Gessler. Wyszły obłędne, koniecznie musicie ich spróbować.',1493368238, 'Przepis na świąteczne ciasteczka.', 8, 7 ),
-            (3, 'Szukam pomysłu na to, czym ozdobić świąteczne ciasto. Lukrem, gotowymi ozdobami, czekoladą? I gdzie kupić takie cudeńka? Z góry dzięki za opdowiedź.', 1493368678, 'Czym ozdobić ciasto?', 6, 2),
-            (4, 'Chciałem na świeta przygotować wypieki dla rodziny, ale popsuł się mój wiekowy mikser. Czy ktoś z Was może polecić jakąś firmę, albo konkrenty model?', 1493368234, 'Gdzie kupić mikser?', 7, 9);
+    VALUES (1, 'Chcę na święta upiec dobry piernik dla 5 osób. Czy ktoś z Was ma jakiś ciekawy przepis dla początkującego kucharza?Pater noster, qui es in caelis, sanctificetur nomen tuum, adveniat regnum tuum, fiat voluntas tua sicut in caelo et in terra.
+Panem nostrum quotidianum da nobis hodie;
+et dimitte nobis debita nostra, sicut et nos dimittimus debitoribus nostris, et ne nos inducas in tentationem, sed libera nos a malo. Amen.', 1493368154, 'Jak zrobić dobry piernik?', 5, 3),
+            (2, 'Cześć. Pierwszy raz zrobiłam dzisiaj świąteczne ciasteczka z przepisu Pani Gessler. Wyszły obłędne, koniecznie musicie ich spróbować.Pater noster, qui es in caelis, sanctificetur nomen tuum, adveniat regnum tuum, fiat voluntas tua sicut in caelo et in terra.
+Panem nostrum quotidianum da nobis hodie;
+et dimitte nobis debita nostra, sicut et nos dimittimus debitoribus nostris, et ne nos inducas in tentationem, sed libera nos a malo. Amen.',1493368238, 'Przepis na świąteczne ciasteczka.', 8, 7 ),
+            (3, 'Szukam pomysłu na to, czym ozdobić świąteczne ciasto. Lukrem, gotowymi ozdobami, czekoladą? I gdzie kupić takie cudeńka? Z góry dzięki za opdowiedź.Pater noster, qui es in caelis, sanctificetur nomen tuum, adveniat regnum tuum, fiat voluntas tua sicut in caelo et in terra.
+Panem nostrum quotidianum da nobis hodie;
+et dimitte nobis debita nostra, sicut et nos dimittimus debitoribus nostris, et ne nos inducas in tentationem, sed libera nos a malo. Amen.', 1493368678, 'Czym ozdobić ciasto?', 6, 2),
+            (4, 'Chciałem na świeta przygotować wypieki dla rodziny, ale popsuł się mój wiekowy mikser. Czy ktoś z Was może polecić jakąś firmę, albo konkrenty model?Pater noster, qui es in caelis, sanctificetur nomen tuum, adveniat regnum tuum, fiat voluntas tua sicut in caelo et in terra.
+Panem nostrum quotidianum da nobis hodie;
+et dimitte nobis debita nostra, sicut et nos dimittimus debitoribus nostris, et ne nos inducas in tentationem, sed libera nos a malo. Amen.', 1493368234, 'Gdzie kupić mikser?', 7, 9);
 
 --
 --
